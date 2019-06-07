b574ec1f1a36c67293d746056edfda2b1a366d62
irisa <iri18shu@gmail.com>
2019-06-07 16:03:55 +0900

    ミス

--- fukusyu0607/Form1.Designer.cs
+++ fukusyu0607/Form1.Designer.cs
@@ -31,12 +31,14 @@
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(113, 95);
+            this.label1.Location = new System.Drawing.Point(58, 63);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 12);
             this.label1.TabIndex = 0;
@@ -45,13 +47,34 @@
             // timer1
             // 
             this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "text";
@@ -64,6 +87,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- fukusyu0607/Form1.cs
+++ fukusyu0607/Form1.cs
@@ -12,18 +12,69 @@ namespace fukusyu0607
 {
     public partial class Form1 : Form
     {
-        int vx = -10;
-        int vy = -10;
+        //疑似乱数
+        //ランダムのシードを指定して初期化したら
+        //それを使い続ける
+        private static Random rand = new Random();
+        int vx = rand.Next(-10, 11);
+        int vy = rand.Next(-10, 11);
+        int vx1 = rand.Next(-10, 11);
+        int vy1 = rand.Next(-10, 11);
+        int vx2 = rand.Next(-10, 11);
+        int vy2 = rand.Next(-10, 11);
+
+        int time = 100 * 5;
 
         public Form1()
         {
             InitializeComponent();
+            //以下に、label1.Leftとlabel1.topの座標をランダムで求める
+            label1.Left = rand.Next(ClientSize.Width - label1.Width);
+            label1.Top = rand.Next(ClientSize.Width - label1.Height);
+
+            label2.Left = rand.Next(ClientSize.Width - label2.Width);
+            label2.Top = rand.Next(ClientSize.Width - label2.Height);
+
+            label3.Left = rand.Next(ClientSize.Width - label3.Width);
+            label3.Top = rand.Next(ClientSize.Width - label3.Height);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
-        {
+        {   
             label1.Left += vx;
             label1.Top += vy;
+
+            label2.Left += vx1;
+            label2.Top += vy1;
+
+            label3.Left += vx2;
+            label3.Top += vy2;
+
+
+
+             //マウスが重なったらタイマーを止める
+            Point p = PointToClient(MousePosition);
+
+             if ((p.X> label1.Left)
+             && (p.X < label1.Right)
+             &&(p.Y > label1.Top)
+             && (p.Y < label1.Bottom))
+
+              timer1.Enabled = false;
+
+             if ((p.X > label2.Left)
+             && (p.X < label2.Right)
+             && (p.Y > label2.Top)
+             && (p.Y < label2.Bottom))
+
+              timer1.Enabled = false;
+
+             if ((p.X > label3.Left)
+              && (p.X < label3.Right)
+              && (p.Y > label3.Top)
+              && (p.Y < label3.Bottom))
+
+              timer1.Enabled = false;
             
             if(label1.Left<0)
             {
@@ -33,7 +84,61 @@ namespace fukusyu0607
             {
                 vy = Math.Abs(vy);
             }
-            
+            if(label1.Right>ClientSize.Width)
+            {
+                vx = Math.Abs(vx);
+            }
+            if(label1.Top>ClientSize.Width)
+            {
+                vx = -Math.Abs(vy);
+            }
+
+            if(label1.Left<ClientSize.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+            if(label1.Bottom<ClientSize.Width)
+            {
+                vy = Math.Abs(vy);
+            }
+            /////////////////////////////////3
+            /////////////////////////////////
+            if(label2.Right>ClientSize.Width)
+            {
+                vx1 = Math.Abs(vx1);
+            }
+            if(label2.Top>ClientSize.Width)
+            {
+                vx1 = -Math.Abs(vy1);
+            }
+
+            if(label2.Left<ClientSize.Width)
+            {
+                vx1 = -Math.Abs(vx1);
+            }
+            if (label2.Bottom < ClientSize.Width)
+            {
+                vy1 = Math.Abs(vy1);
+            }
+            /////////////////////////////////////3
+            /////////////////////////////////////
+            if (label3.Right > ClientSize.Width)
+            {
+                vx2 = Math.Abs(vx2);
+            }
+            if (label3.Top > ClientSize.Width)
+            {
+                vx2 = -Math.Abs(vy2);
+            }
+
+            if (label3.Left < ClientSize.Width)
+            {
+                vx2 = -Math.Abs(vx2);
+            }
+            if (label3.Bottom < ClientSize.Width)
+            {
+                vy2 = Math.Abs(vy2);
+            }
             
         }
     }
